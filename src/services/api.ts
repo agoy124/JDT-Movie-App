70aaae947e5e3ae9de3f31dd7b55cb0da56eb59f
JDT16 Prayoga Hajid Mu'arif <jdt16.prayoga.muarif@gmail.com>
2025-09-29 10:23:43 +0700

    benerin

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 let bearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMThjNzczZjczMmJlNmI3ZGFhZTllMWZlYTZjNzgxYiIsIm5iZiI6MTc1ODI2NDgwNy4xMjcsInN1YiI6IjY4Y2NmZGU3YzRhNjJmMDljYmNmNGIyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UMoGtDXOFt-f_yKjVqAaLZyZZtiHtm9tIAnQ2k0c9Bw"
 const API = axios.create();
 
-// export const setAxiosConfig = (token: string) => {
-//   // bearerToken = token;
-// };
+export const setAxiosConfig = (bearerToken: string) => {
+  bearerToken = bearerToken;
+};
 
 API.interceptors.request.use((axiosConfig) => {
   axiosConfig.baseURL = import.meta.env.VITE_BASE_URL;
